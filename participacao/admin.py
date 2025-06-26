a9a33bfb9e54cce60f3e1783627220fc4bd4a6c4
Time1-software <time1.proj.software@gmail.com>
2025-06-25 21:02:07 -0300

    Delete participacao directory

--- participacao/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Aluno
-
-@admin.register(Aluno)
-class AlunoAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'matricula', 'turma')
-    search_fields = ('nome', 'matricula', 'turma')
